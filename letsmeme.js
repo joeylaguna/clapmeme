e7d8cccfe897b7d015c1e59b099b494d5c7a83f2
jlaguna <elagunaj@gmail.com>
2016-11-30 13:44:07 -0800

    added text to speach on click

--- letsmeme.js
+++ letsmeme.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   var $memeSection = $("<div class='memeSection'></div>");
   $memeSection.appendTo('body');
 
+
+
   $(document.body).on('click', '.memeStr', function() {
   	var checked = $(".caps").prop('checked');
   	var spaceChecked = $(".space").prop('checked');
@@ -17,6 +19,34 @@ $(document).ready(function(){
       	}
       }
     });
+
     $(".memeSection").html(newMeme.join(''));
-  });
+
+    var clapsound = new Audio('clap.wav');
+    
+    function clapsay(text) {
+      var msg = new SpeechSynthesisUtterance();
+      var voices = window.speechSynthesis.getVoices();
+      msg.voice = voices[10]; // Note: some voices don't support altering params
+      msg.voiceURI = 'native';
+      msg.volume = 1; // 0 to 1
+      msg.rate = 1; // 0.1 to 10
+      msg.pitch = 1; //0 to 2
+      msg.text = text;
+      msg.lang = 'en-US';
+
+      msg.onend = function(event) {
+        console.log('Finished in ' + event.elapsedTime + ' seconds.');
+      };
+      
+      msg.onboundary = function(event) {
+        console.log('boundary');
+        clapsound.play();
+      }
+      
+      speechSynthesis.speak(msg);
+    }
+    clapsay(newMeme.join(''));
+    });
 });
+
