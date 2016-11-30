cf6dbf15cc288c67007459355d92fad2c59e77c0
joeylaguna <elagunaj@gmail.com>
2016-11-29 20:17:19 -0800

    refactor

--- letsmeme.js
+++ letsmeme.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
   var $memeSection = $("<div class='memeSection'></div>");
-  $memeSection.appendTo('body'); 
+  $memeSection.appendTo('body');
 
-  $(document.body).on('click','.memeStr',function(){
-    var original = $('.meme').val();
-    var arr = original.split('');
-    var newMeme = [];
-    arr.forEach(function(value){
-      if(value===' '){
-        newMeme.push(' \uD83D\uDC4F ');
+  $(document.body).on('click', '.memeStr', function() {
+    var original = $('.meme').val().split('');
+    var newMeme = original.map(function(value) {
+      if(value === ' '){
+        return ('\uD83D\uDC4F');
       }else{
-      	newMeme.push(value);
+        return value;
       }
     });
     $(".memeSection").html(newMeme.join(''));
